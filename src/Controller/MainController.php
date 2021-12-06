6f2501e5a04b597018bf1ba6471ee24a0f659098
SimonDelaporte1 <simondelaporte@gmail.com>
2021-12-06 15:05:17 +0100

    route avec annotations

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,12 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+// un use est nécessaire pour les @route
+use Symfony\Component\Routing\Annotation\Route;
+
+
 
 Class MainController
 {
+    /** 
+     * @Route("/", name="main_home")
+     */
     public function home()
     {
         // affiche quelque chose
         return new Response("Bienvenue sur O'flix");
+    }
+
+    /** 
+     * @Route("/movie/{id}", name="main_movie_show")
+     */
+    public function movieShow($id)
+    {
+        // affiche quelque chose
+        return new Response("Page du film/série dont l'id est : " . $id);
     }
 }
