6ac3420bb7d70e3b4d992d4abd219c87220a2436
SimonDelaporte1 <simondelaporte@gmail.com>
2021-12-22 11:16:37 +0100

    ajout controller user + correction ACL front

---
+++ src/Controller/Back/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * On peut préfixer toutes les routes de ce contrôleur
+ * 
+ * @Route("/back/user")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/", name="back_user_index", methods={"GET"})
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('back/user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="back_user_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Utilisateur ajouté(e).');
+
+            return $this->redirectToRoute('back_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="back_user_show", methods={"GET"})
+     * 
+     * $user = null permet de récupérer la main sur la 404
+     */
+    public function show(User $user = null): Response
+    {
+        if ($user === null) {
+            throw $this->createNotFoundException('Utilisateur non trouvé(e).');
+        }
+
+        return $this->render('back/user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="back_user_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Utilisateur modifié(e).');
+
+            return $this->redirectToRoute('back_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="back_user_delete", methods={"POST"})
+     */
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        $this->addFlash('success', $user->getEmail() . ', supprimé.');
+
+        return $this->redirectToRoute('back_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
