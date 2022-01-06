2e1bd0da535141f4d44dbaac5d1f43d9dfa820be
SimonDelaporte1 <simondelaporte@gmail.com>
2022-01-06 10:09:40 +0100

    correction challenge subscriber maintenance

--- src/EventSubscriber/MaintenanceSubscriber.php
+++ src/EventSubscriber/MaintenanceSubscriber.php
@@ -9,9 +9,25 @@ class MaintenanceSubscriber implements EventSubscriberInterface
 {
     public function onKernelResponse(ResponseEvent $event)
     {
+        if (!$event->isMainRequest()) {
+            // don't do anything if it's not the main request
+            return;
+        }
+        
+        // Si URL du Profiler ou de la WDT, on sort
+        // $request->getPathInfo() contient la route
+        if (preg_match('/^\/(_profiler|_wdt)/', $event->getRequest()->getPathInfo())) {
+            return;
+        }
+
+        // Requête XHR/Fetch ? (AJAX)
+        if ($event->getRequest()->isXmlHttpRequest()) {
+            return;
+        }
+
         $response = $event->getResponse();
         $content = $response->getContent();
-        $content = str_replace('<body>','<body><div class="alert alert-danger">Maintenance prévue mardi 10 janvier à 17h00</div>', $content);
+        $content = str_replace('</nav>','</nav><div class="alert alert-danger">Maintenance prévue mardi 10 janvier à 17h00</div>', $content);
         $response->setContent($content);
     }
 
